743bd5b7eb91f3ab751dd9ef7660eec055453ce1
Jack Mallahan <jackmallahan@gmail.com>
2017-07-19 17:20:47 -0600

    added user interface

--- scripts/index.js
+++ scripts/index.js
@@ -1,10 +1,22 @@
 import Trie from '../scripts/Trie'
-import Node from '../scripts/Node'
-
-
-console.log('loaded');
+import words from './words'
+import $ from 'jquery'
 
 const newTree = new Trie()
 
-newTree.populate(dictionary)
-console.log(newTree)
+newTree.populate(words)
+
+console.log(newTree.suggest('pizza'));
+
+$('.submit-btn').on('click', () => {
+
+  $('.append-section').append(`
+
+
+    newTree.suggest($('.user-input').val())
+    `)
+} )
+
+function search() {
+  
+}
